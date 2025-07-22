d7b602dc52f35290858d0f3f35b69fa7b45c1780
flammable121 <flammable121@gmail.com>
2025-07-22 05:11:16 +0300

    Вроде готовая верстка

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,61 @@
 <template>
     <div class="converter">
-        <h3 class="converter__title">Конвертер валют</h3>
-        <div class="converter__box">
-            <currency-input></currency-input>
-            <div class="converter_array">
-                <svg xmlns="http://www.w3.org/2000/svg" width="70" height="88" viewBox="0 0 70 88" fill="none">
-                    <path d="M23.6443 48.4825L45.6784 26.4483C46.0689 26.0578 46.069 25.4247 45.6784 25.0341L23.6443 2.99999" stroke="black" stroke-width="4" stroke-linecap="square"/>
-                    <path d="M46.5707 39.6822L24.5365 61.7163C24.146 62.1069 24.146 62.74 24.5365 63.1305L46.5707 85.1647" stroke="black" stroke-width="4" stroke-linecap="square"/>
-                </svg>
+        <div class="converter__inner">
+            <h3 class="converter__title">Конвертер валют</h3>
+            <div class="converter__box">
+                <currency-input/>
+                <div class="converter__array">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="70" height="88" viewBox="0 0 70 88" fill="none">
+                        <path d="M23.6443 48.4825L45.6784 26.4483C46.0689 26.0578 46.069 25.4247 45.6784 25.0341L23.6443 2.99999" stroke="black" stroke-width="4" stroke-linecap="square"/>
+                        <path d="M46.5707 39.6822L24.5365 61.7163C24.146 62.1069 24.146 62.74 24.5365 63.1305L46.5707 85.1647" stroke="black" stroke-width="4" stroke-linecap="square"/>
+                    </svg>
+                </div>
+                <currency-input />
             </div>
-            <currency-input></currency-input>
         </div>
-    </div>    
+    </div>
 </template>
 
 <script setup>
 import CurrencyInput from './componenets/CurrencyInput.vue';
-
 </script>
 
 <style lang="scss">
-body {
+* {
+    font-family: "Gothic";
     margin: 0;
     padding: 0;
     box-sizing: border-box;
 }
 .converter {
-    & h3 {
-        
-        font-size: 64px;
-    }
-
-}
-.converter_array {
-
-}
-.converter__box {
     display: flex;
+    justify-content: center;
     align-items: center;
+    height: 100vh;
+    width: 100vw;
 
+    .converter__inner {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        max-width: 110rem;
+
+        .converter__title {
+            font-size: 4rem;
+            margin: 3.9rem 0;
+        }
+    
+        .converter__box {
+            display: flex;
+            justify-content: space-between;
+
+            .converter__array {
+                display: flex;
+                justify-content: center;
+                align-items: end;
+                padding-bottom: 7.5rem;
+            }
+        }
+    }
 }
-
-@media (max-width: 800px) {
-
-}
-
 </style>
--- src/componenets/CurrencyInput.vue
+++ src/componenets/CurrencyInput.vue
@@ -5,17 +5,17 @@
             <button class="currency--btn">USD</button>
             <button class="currency--btn">USD</button>
             <button class="currency--btn">USD</button>
-            <span 
-            class="currency--btn">
+            <span class="currency--array">
                 <svg xmlns="http://www.w3.org/2000/svg" width="52" height="28" viewBox="0 0 52 28" fill="none">
                     <path d="M3.18811 3.00348L25.2222 25.0376C25.6128 25.4281 26.2459 25.4281 26.6365 25.0376L48.6706 3.00348" stroke="#C05406" stroke-width="4" stroke-linecap="square"/>
                 </svg>
           </span>
+          <drop-currency/>
         </div>
     <div class="convertor__block--input">
         <p class="input__p">Казахстанский тенге</p>
-        <input class="input__input" type="text" v-model="amountTo">
-        <p class="input__currency">1 USD = 468.27 KZT</p>
+        <input class="input__input" type="number" v-model="amountTo">
+        <p class="input__p">1 USD = 468.27 KZT</p>
     </div>
 </div>
 </template>
@@ -26,51 +26,57 @@
 
 <style lang="scss" scoped>
 .convertor__input {
-    width: 788px;
-    & {
-        font-size: 36px;
-        color: #C05406;
-    }
-
+  max-width: 45.875rem;
+  font-size: 2.25rem;
+  box-sizing: border-box;
+  color: #C05406;
+  .convertor__input--name {
+    margin-top: 0;
+    margin-bottom: 3.75rem;
+  }
 }
 
 .convertor__input--currency {
-    display: flex;
-    justify-content: space-between;
-    span,
-    button {
+  display: flex;
+  justify-content: start;
+  width: 100%;
+  .currency--btn {
     border: 1px solid #CDCDCD;
-    font-size: 36px;
-    padding: 29px 60px;
+    font-size: 2.25rem;
+    padding: 1.1875rem 3.75rem;
     position: relative;
-    text-align: center;
-    justify-content: center;
-    overflow: hidden;
-    width: 100%;
+    width: 12.3125rem;
+    height: 5.3rem;
     &:focus {
-        background-color: #C05406;
+      background-color: #C05406;
     }
+  }
+  .currency--array {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid #CDCDCD;
+    width: 8.875rem;
+  }
 }
-}
-
 
 .convertor__block--input {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
+  background-color: #F1F1F1;
+  margin-top: 2rem;
 
-    & p {
-        font-size: 24px;
-        color: black;
-    }
+  .input__p {
+    font-size: 1.5rem;
+    color: black;
+    margin: 0;
+    padding: 2.4rem 0.9rem ;
+  }
 
-    & input {
-        font-size: 100px;
-        border: none;
-    }
+  .input__input {
+    font-size: 6.25rem;
+    border: none;
+    background-color: #F1F1F1;
+  }
 }
-
-.input__currency {
-    font-size: 24px;
-}
-
 </style>
